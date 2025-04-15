b11ddc8bb4a80f6a914825cce9387f17e3bf6c0d
rohankar7 <rohankar365@gmail.com>
2025-04-15 22:16:02 +0100

    Added: Problem 463

--- IslandPerimeter.py
+++ IslandPerimeter.py
@@ -1,6 +1,16 @@
-# 463. Island Perimeter
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        for i in grid:
-            for j in i:
-                print(j)
+        perimeter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j]==1:
+                    perimeter += 4
+                    if i>0 and grid[i-1][j]==1:
+                        perimeter -= 1
+                    if i<len(grid)-1 and grid[i+1][j]==1:
+                        perimeter -= 1
+                    if j>0 and grid[i][j-1]==1:
+                        perimeter -= 1
+                    if j<len(grid[i])-1 and grid[i][j+1]==1:
+                        perimeter -= 1
+        return perimeter
