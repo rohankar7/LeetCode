7f14b1170d337695f2ea25eebd4322c62fd99e55
rohankar7 <rohankar365@gmail.com>
2025-04-21 22:12:58 +0100

    Added: Problem 409

--- LongestPalindrome.py
+++ LongestPalindrome.py
@@ -1,1 +1,20 @@
 # 409. Longest Palindrome
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hashmap = {}
+        size = 0
+        addedOdd = False
+        for i in s:
+            if hashmap.get(i)==None:
+                hashmap[i]=1
+            else:
+                hashmap[i]+=1
+        for i in hashmap.values():
+            if (i%2==0):
+                size += i
+            else:
+                size += i-1
+                addedOdd = True
+        if addedOdd:
+            size += 1
+        return size
---
+++ demo.py
@@ -0,0 +1,2 @@
+d = {"1":"Haha", "2":"Hey"}
+print(list(d.values())[::-1])
