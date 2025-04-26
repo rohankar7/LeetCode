e34f7a2cbee818b94aa1358ae26fce5d06a5a99c
rohankar7 <rohankar365@gmail.com>
2025-04-26 23:35:28 +0100

    Added: Problem 3498

--- ReverseDegreeOfAString.py
+++ ReverseDegreeOfAString.py
@@ -1,8 +1,9 @@
 # 3498. Reverse Degree of a String
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        sum = 0
-        asciia = ord('a') + 26
-        for i in range(len(s)):
-            sum += (asciia-ord(s[i]))*(i+1)
-        return sum
+        # sum = 0
+        # asciia = ord('a') + 26
+        # for i in range(len(s)):
+        #     sum += (asciia-ord(s[i]))*(i+1)
+        # return sum
+        return sum([(ord('a') + 26 - ord(s[i])) * (i+1) for i in range(len(s))])
