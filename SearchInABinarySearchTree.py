a173c76dd3ca60859cd4d6bcd7515ff1236488ac
rohankar7 <rohankar365@gmail.com>
2025-05-07 18:40:18 +0100

    Added: Problem 700

---
+++ SearchInABinarySearchTree.py
@@ -0,0 +1,13 @@
+# 700. Search in a Binary Search Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root: return None
+        if root.val == val: return root
+        elif val > root.val: return self.searchBST(root.right, val)
+        elif val < root.val: return self.searchBST(root.left, val)
