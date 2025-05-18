fdaec08afb1122f7f5aeac25497a95e4fc925687
rohankar7 <rohankar365@gmail.com>
2025-05-18 22:53:42 +0100

    Added: Problem 594

--- LongestHarmoniousSubsequence.py
+++ LongestHarmoniousSubsequence.py
@@ -1,1 +1,14 @@
 # 594. Longest Harmonious Subsequence
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        nums.sort()
+        hash = {}
+        for i in nums:
+            hash[i] = hash.get(i, 0) + 1
+        maxSize = 0
+        k = list(hash.keys())
+        v = list(hash.values())
+        for i in range(len(hash)-1):
+            if k[i+1] == k[i] + 1:
+                maxSize = max(maxSize, v[i]+v[i+1])
+        return maxSize
