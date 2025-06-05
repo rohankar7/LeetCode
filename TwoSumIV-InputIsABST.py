6a45d023b0df710d08b6c6e6309b73a7ca581dc0
rohankar7 <rohankar365@gmail.com>
2025-06-05 23:47:12 +0100

    Added: Problem 653

--- TwoSumIV-InputIsABST.py
+++ TwoSumIV-InputIsABST.py
@@ -1,1 +1,21 @@
 # 653. Two Sum IV - Input is a BST
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        hash = {}
+        def inorder(node):
+            if not node: return
+            hash[node.val] = hash.get(node.val, 0) + 1
+            inorder(node.left)
+            inorder(node.right)
+        inorder(root)
+        for key, val in hash.items():
+            if k-key in hash.keys():
+                if k-key==key and val<2: continue
+                else: return True
+        return False
